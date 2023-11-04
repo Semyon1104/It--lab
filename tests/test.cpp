77ff59ba9e15c135370c913cf2cd22545be519f0
Semyon1104 <sema_titov_04@mail.ru>
2023-11-04 13:23:44 +0300

    3

--- tests/test.cpp
+++ tests/test.cpp
@@ -10,8 +10,15 @@ TEST(test_group, test_1) {
 	c = sum(a, b);      //Act
 	ASSERT_EQ(c, a + b);     //Assert
 }
-int main() {
-	::testing::InitGoogleTest(&args, argv);
+
+TEST(test_group, test_2) {
+	int a = 5, b = 6;
+	int c;
+	c = dif(a, b);
+	ASSERT_EQ(c, a - b);
+}
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 	
 }
